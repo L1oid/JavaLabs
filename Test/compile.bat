ed87d3a3e1260a4211147d68c10a376102cfa7bc
L1oid <71085120+L1oid@users.noreply.github.com>
2022-03-07 19:48:03 +0700

    Add files via upload

--- Test/compile.bat
+++ Test/compile.bat
@@ -1,8 +1,3 @@
-"C:\Users\lloid\.jdks\openjdk-17.0.2\bin\javac.exe" exceptions\ExcpRadius.java
-"C:\Users\lloid\.jdks\openjdk-17.0.2\bin\javac.exe" figure\Figure.java
-"C:\Users\lloid\.jdks\openjdk-17.0.2\bin\javac.exe" figure\circle\Circle.java
-"C:\Users\lloid\.jdks\openjdk-17.0.2\bin\javac.exe" figure\rectangle\Rectangle.java
-"C:\Users\lloid\.jdks\openjdk-17.0.2\bin\javac.exe" figure\cylinder\Cylinder.java
-"C:\Users\lloid\.jdks\openjdk-17.0.2\bin\javac.exe" main\Main.java
+"C:\Users\lloid\.jdks\openjdk-17.0.2\bin\javac.exe" -cp .;..;. main\Main.java
 
 pause
