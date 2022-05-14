2feddce4f31fd78487cb9fa0ff2f6b81c82e9482
L1oid <71085120+L1oid@users.noreply.github.com>
2022-05-14 10:42:54 +0700

    update

--- LabDataBase/Main.java
+++ LabDataBase/Main.java
@@ -11,12 +11,9 @@ public class Main extends Application{
      
     @Override
     public void start(Stage stage) throws Exception {
-         
         Parent root = FXMLLoader.load(getClass().getResource("./view/view.fxml"));
         Scene scene = new Scene(root);
-         
         stage.setScene(scene);
-         
         stage.setTitle("Car");
         stage.show();
     }
